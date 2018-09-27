70b5b21bbbe5814ad9471f3601dcd89d962e195b
Morten Stulen <enstulen@gmail.com>
2018-09-27 14:44:46 +0200

    Add support for adding/removing/editing preferences

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Switch between options
 $(document).ready(function() {
   $('.header__selector').click(function(event) {
     $('.header__selector__container')
@@ -12,3 +13,26 @@ $(document).ready(function() {
     }
   });
 });
+
+// Add a new preference
+function plusButtonPressed() {
+  var container = document.getElementById('preferences__selections__container');
+  const inputs = document.querySelectorAll('.preferences__selection__input');
+
+  for (const input of inputs) {
+    if (input.innerHTML === '') {
+      return;
+    }
+  }
+
+  var child =
+    ' <div class="preferences__selection"> <span class="preferences__selection__input" contenteditable="true"></span> <img class="preferences__selection__img" onclick="deletePreferenceButtonPressed(this)" src="images/icons/cross.png" alt="preferenes" /> </div>';
+  container.insertAdjacentHTML('afterbegin', child);
+}
+
+// Delete selected preference
+function deletePreferenceButtonPressed(element) {
+  var container = document.getElementById('preferences__selections__container');
+  var parent = element.parentNode;
+  container.removeChild(parent);
+}
